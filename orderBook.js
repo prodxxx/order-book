74195f613f2944d6d77ef265129418beb197308f
DaveM <dave.miranda@gmail.com>
2020-03-22 17:18:46 -0400

    orderbook first pass

---
+++ orderBook.js
@@ -0,0 +1,36 @@
+
+
+const reconcileOrder = (existingBook,incomingOrder) => {
+  
+  for (let i = 0 ; i < existingBook.length; i++) {
+    if (incomingOrder.type !== existingBook[i].type) {
+      if (existingBook[i].type === 'sell' && incomingOrder.price >= existingBook[i].price) {
+        if (existingBook[i].quantity >= incomingOrder.quantity) {
+          existingBook[i].quantity -= incomingOrder.quantity
+          incomingOrder.quantity = 0
+        }
+        else {
+          incomingOrder.quantity -= existingBook[i].quantity
+          existingBook[i].quantity = 0
+        }
+      }
+      else if (existingBook[i].type === 'buy' && incomingOrder.price <= existingBook[i].price) {
+        if (existingBook[i].quantity >= incomingOrder.quantity) {
+          existingBook[i].quantity -= incomingOrder.quantity
+          incomingOrder.quantity = 0
+        }
+        else {
+          incomingOrder.quantity -= existingBook[i].quantity
+          existingBook[i].quantity = 0
+        }
+      }
+    }  
+  }
+  if (incomingOrder.quantity > 0) {
+    existingBook.push(incomingOrder)
+  }
+  return existingBook
+}
+
+
+module.exports = reconcileOrder
